e6b1261f6abba93832a422c2dbc4c39f5dc3a897
Midhunan D J <your_github_midhunandj.eee2024@citchennai.net>
2026-03-01 22:59:18 +0530

    Removed unused experimental scripts

--- speech_to_text.py
+++
@@ -1,9 +0,0 @@
-import nemo.collections.asr as nemo_asr
-
-print("Loading model...")
-
-model = nemo_asr.models.EncDecCTCModel.from_pretrained(
-    model_name="QuartzNet15x5Base-En"
-)
-
-print("Model loaded successfully!")
